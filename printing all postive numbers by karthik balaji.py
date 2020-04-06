6710ca89ede878023743445cada972274762c7fa
Joker-99 <63245513+Joker-99@users.noreply.github.com>
2020-04-06 20:00:37 +0530

    Add files via upload

---
+++ printing all postive numbers by karthik balaji.py
@@ -0,0 +1,15 @@
+# giving the first list as input
+list_1 =[12, -7, 5, 64, -14]
+
+# setting up the code
+for x in list_1:
+        if x>=0:
+            print(x)
+
+# giving the secong list as input
+list_2 = [12, 14, -95, 3]
+
+#setting up the code
+for y in list_2:
+    if y>=0:
+        print(y)
